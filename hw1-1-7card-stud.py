8df640541eee072fa090163025b6e03b60ccb054
saurabhs92 <saurabhs92@gmail.com>
2016-02-20 02:17:38 +0530

    New note on itertools.combinations. Completed work on 7-card stud.

--- hw1-1-7card-stud.py
+++ hw1-1-7card-stud.py
@@ -3,13 +3,18 @@
 # Homework Assigment 1 
 # Write the best_hand function which takes in a 7 card hand and returns the best 5 card hand. 
 
+from poker import hand_rank
+import itertools
+
 def best_hand(hand):
     "From a 7 card hand, return the best 5 card hand."
+    return max(itertools.combinations(hand, 5), key=hand_rank)
 
 def test_best_hand():
-    assert (sorted(best_hand("6C 7C 8C 9C TC 5C JS".split())) == ['6C', '7C', '8C', '9C', 'TC']) 
+    assert (sorted(best_hand("6C 7C 8C 9C TC 5C JS".split())) 
+            == ['6C', '7C', '8C', '9C', 'TC']) 
     assert (sorted(best_hand("TD TC TH 7C 7D 8C 8S".split())) 
-           == ['8C', '8C', 'TC', 'TD', 'TH']) 
+           == ['8C', '8S', 'TC', 'TD', 'TH']) 
     assert (sorted(best_hand("TD TC TH 7C 7D 7S 7H".split())) 
            == ['7C', '7D', '7H', '7S', 'TD']) 
     return 'test_best_hand passes'
