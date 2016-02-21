ff16fccb83090ee1deb8cbab2c70401982d98128
saurabhs92 <saurabhs92@gmail.com>
2016-02-21 21:24:00 +0530

    Solved zebra_puzzle by correct, faster approach. Takes < 1 sec.

--- zebra_puzzle.py
+++ zebra_puzzle.py
@@ -37,18 +37,32 @@ def imright(h1, h2):
 def nextto(h1, h2):
     "Two houses are next to each other if they differ by 1."
     return abs(h1-h2) == 1
-
-# Bad approach - will take about an hour to solve by this method:
-for (red, green, ivory, yellow, blue) in orderings:
-    for (Englishman, Spaniard, Ukranian, Japanese, Norwegnian) in orderings:
-        for (dog, snails, fox, horse, zebra) in orderings:
-            for (coffee, tea, milk, oj, water) in orderings:
-                for (OldKools, Kools, Chesterfields, LuckyStrike, Parliaments) in orderings:
-                    # constraints here
-                    
-                
-            
-        
     
+def zebra_puzzle():
+    "Return a tuple (WATER, ZEBRA) indicating their house numbers."
+    houses = first, _, middle, _, _ = [1, 2, 3, 4, 5]
+    orderings = list(itertools.permutations(houses)) # 1
+    return next((WATER, ZEBRA)
+                for (red, green, ivory, yellow, blue) in orderings
+                if imright(green, ivory)       # 6
+                for (Englishman, Spaniard, Ukranian, Japanese, Norwegnian) in orderings 
+                if (Englishman is red)         # 2
+                if nextto(Norwegnian, blue)  #15
+                if (Norwegnian is first)       #10
+                for (dog, snails, fox, horse, ZEBRA) in orderings
+                if (Spaniard is dog)           # 3
+                for (coffee, tea, milk, oj, WATER) in orderings
+                if (Ukranian is tea)           # 5
+                if (milk is middle)            # 9
+                if (coffee is green)           # 4
+                for (OldGold, Kools, Chesterfields, LuckyStrike, Parliaments) in orderings
+                if (OldGold is snails)         # 7
+                if (Kools is yellow)           # 8
+                if nextto(Chesterfields, fox)  #11
+                if nextto(Kools, horse)        #12
+                if (LuckyStrike is oj)         #13
+                if (Japanese is Parliaments)   #14
+                )
 
-
+WATER, ZEBRA = zebra_puzzle()
+print WATER, ZEBRA
