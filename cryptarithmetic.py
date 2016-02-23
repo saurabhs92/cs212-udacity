9b8e8914b2a9ea5d979f0974e3e83a4adb9bc505
saurabhs92 <saurabhs92@gmail.com>
2016-02-24 01:55:07 +0530

    Added compile_word() which substitutes a formula for uppercase words.

--- cryptarithmetic.py
+++ cryptarithmetic.py
@@ -25,7 +25,7 @@ We will try substituting letters with all combinations of digits and use eval to
 
 from __future__ import division
 from zebra_puzzle import timecall
-import string, re, itertools, time
+import string, re, itertools, time, cProfile
 
 def solve(formula):
     """Given a formula like 'ODD + ODD == EVEN', fill in digits to solve it.
@@ -49,6 +49,16 @@ def valid(f):
     except ArithmeticError:    # Division-by-zero, overflows, etc.
         return False
 
+def compile_word(word):
+    """Complle a word of uppercase letters as digits.
+    E.g., compile_word('YOU') => '(1*U + 10*O + 100*Y)'
+    Non-uppercase words unchanged: compile_word('+') => '+'"""
+    if(word.isupper()):
+        terms = ['%s*%s' % (10**i, d) for i, d in (enumerate(word[::-1]))]
+        return '(' + '+'.join(terms) + ')'
+    else:
+        return word
+
 examples = """ TWO + TWO == FOUR
 A**2 + B**2 == C**2
 A**2 + BE**2 == CY**2
@@ -71,4 +81,4 @@ def test():
         print '%6.4f sec :  %s' % timecall(solve, example)
     print '%6.4f tot.' % (time.clock() - t0)
 
-test()
+cProfile.run('test()')
