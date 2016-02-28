88b21b7d3e51041832358b2b8e82e9c09ac900be
saurabhs92 <saurabhs92@gmail.com>
2016-02-28 23:33:47 +0530

    Added definitions of match_star and match1 fns.

--- re.py
+++ re.py
@@ -18,13 +18,13 @@ def search(pattern, text):
 def match(pattern, text):
     "Return True if pattern appears at the start of the text."
     if (pattern == ''):
-        return True
+        return True 
     elif pattern == '$':
         return (text == '')
     elif len(pattern) > 1 and pattern[1] in '*?':
         p, op, pat = pattern[0], pattern[1], pattern[2:]
         if (op == '*'):
-            return match_star(p, pattern, text)
+            return match_star(p, pat, text)
         elif (op == '?'):
             if (match1(p, text) and match(pat, text[1:])):
                 return True
@@ -33,6 +33,19 @@ def match(pattern, text):
     else:
         return (match1(pattern[0], text) and 
                 match(pattern[1:], text[1:]))
+    
+def match1(p, text):
+    """Return true if first character of text matches
+    pattern character p."""
+    if not text: return False
+    return p == '.' or p == text[0]
+
+def match_star(p, pattern, text):
+    """Return True if any no. of p followed by 
+    patttern, matches text."""
+    return (match(pattern, text) or 
+            match1(p, text) and 
+            match_star(p, pattern, text[1:]))
     
         
 def test():
@@ -49,12 +62,12 @@ def test():
     assert match('x?', 'text') 
     assert match('text?', 'text')
     assert match('text?', 'text')
-    def words(text): return words.split()
+    def words(text): return text.split()
     assert all(match('aa*bb*cc*$', s) for s in words('abc aabbcc aaabcccc'))
-    assert not any(match('aa*bb*cc*$', s) for s in words('abc aabbcc aaabcccc'))
-    assert all(match('^ab.*aca.*a$', s) for s in words('abracadabra abacaa about-acacia-fa'))
-    assert all(match('t.p', s) for s in words('tip top tap atypical tepid stop'))
-    assert not any(match('t.p', s) for s in words('TYPE teepee tp'))
+    assert not any(match('aa*bb*cc*$', s) for s in words('ac aabbccd aaa-b-cccc'))
+    assert all(search('^ab.*aca.*a$', s) for s in words('abracadabra abacaa aboutacaciafa'))
+    assert all(search('t.p', s) for s in words('tip top tap atypical tepid stop'))
+    assert not any(search('t.p', s) for s in words('TYPE teepee tp'))
     return 'test passes'
 
-
+print test()
