c55176fe480bf284df19ca20310dd6d8d91caa6a
saurabhs92 <saurabhs92@gmail.com>
2016-02-28 22:13:24 +0530

    Added definition of match(). Assumes defn of match_star and match1 fns.

--- re.py
+++ re.py
@@ -17,7 +17,24 @@ def search(pattern, text):
 
 def match(pattern, text):
     "Return True if pattern appears at the start of the text."
+    if (pattern == ''):
+        return True
+    elif pattern == '$':
+        return (text == '')
+    elif len(pattern) > 1 and pattern[1] in '*?':
+        p, op, pat = pattern[0], pattern[1], pattern[2:]
+        if (op == '*'):
+            return match_star(p, pattern, text)
+        elif (op == '?'):
+            if (match1(p, text) and match(pat, text[1:])):
+                return True
+            else:
+                return match(pat, text)
+    else:
+        return (match1(pattern[0], text) and 
+                match(pattern[1:], text[1:]))
     
+        
 def test():
     assert search('baa*!', 'Sheep said baaa!')
     assert search('baa*!', 'Sheep said baaaa humbug') == False
