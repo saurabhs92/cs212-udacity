a90add36566a185014cc2c99d5134d90aed981d7
saurabhs92 <saurabhs92@gmail.com>
2016-03-01 00:11:23 +0530

    Added more assertion tests in test_search().

--- re_api.py
+++ re_api.py
@@ -16,6 +16,14 @@ def test_search():
     assert search(seq(lit('def'), eol), 'abcdef') == 'def'
     assert search(a, 'not the start') == 'a'
     assert match(a, 'not the start') == None
-    assert match(abcstars, 'aaabbccccccdef') == aaabbcccccc
+    assert match(abcstars, 'aaabbccccccdef') == 'aaabbcccccc'
     assert match(abcstars, 'junk') == ''
-    
+    assert all(match(seq(abcstars, eol), s) == s for s in 'abc aaabbccc aaaabcccccc'.split())
+    assert all(match(seq(abcstars, eol), s) == None for s in 'cab aaabbcccd aaaa-b-cccccc'.split())
+    r = seq(lit('ab'), seq(dotstar, seq(lit('aca'), seq(dotstar, seq(a, eol)))))
+    assert all(search(r, s) is not None for s in 'abracadabra abaca about-acacia-flora'.split())
+    assert all(match(seq(c, seq(dot, b)), s) is not None for s in 'cab cob carob cb carbuncle'.split())
+    assert not any(match(seq(c, seq(dot, b)), s) for s in 'crab cb across scab'.split())
+    return 'test_search passes.'
+
+
