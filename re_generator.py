9ed527b6cdb1cd9c46458d596cac436c30b0d8ed
saurabhs92 <saurabhs92@gmail.com>
2016-03-12 01:51:26 +0530

    Optimized compiler by moving statements to the left of lambda.

--- re_generator.py
+++ re_generator.py
@@ -8,9 +8,14 @@ Regular Expressions - Generators
 
 """
 
-def lit(s): return lambda Ns: set([s]) if len(Ns) in Ns else null
-def alt(x, y): return lambda Ns: x(Ns) | y(Ns)
-def star(x): return lambda Ns: opt(plus(x))(Ns)
-def opt(x): return alt(epsilon, x)
-dot = oneof('?')
-epsilon = lit('')
+def lit(s):       
+    set_s = set([s])
+    return lambda Ns: set_s if len(Ns) in Ns else null
+def alt(x, y):    return lambda Ns: x(Ns) | y(Ns)
+def star(x):      return lambda Ns: opt(plus(x))(Ns)
+def oneof(chars): 
+    set_chars = set(chars)
+    return lambda Ns: set_chars if 1 in Ns else null
+def opt(x):       return alt(epsilon, x)
+dot = oneof('?')  # Add as many characters as you want
+epsilon = lit('') # The pattern which matches empty string.
