a8f95ce9bf8b5bda1bc24052c2286b3ac95928c5
saurabhs92 <saurabhs92@gmail.com>
2016-03-13 23:15:26 +0530

    Added definitions of seq and plus functions.

--- re_generator.py
+++ re_generator.py
@@ -17,5 +17,7 @@ def oneof(chars):
     set_chars = set(chars)
     return lambda Ns: set_chars if 1 in Ns else null
 def opt(x):       return alt(epsilon, x)
+def seq(x, y):    return lambda Ns: genseq(x, y, Ns)
+def plus(x):      return lambda Ns: genseq(x, star(x), Ns, startx=1)
 dot = oneof('?')  # Add as many characters as you want
 epsilon = lit('') # The pattern which matches empty string.
