0b3287980d3e4f7a7d22725dc465c35589b7271e
saurabhs92 <saurabhs92@gmail.com>
2016-03-15 23:29:00 +0530

    Added function genseq and a testcase in test_gen().

--- re_generator.py
+++ re_generator.py
@@ -21,3 +21,23 @@ def seq(x, y):    return lambda Ns: genseq(x, y, Ns)
 def plus(x):      return lambda Ns: genseq(x, star(x), Ns, startx=1)
 dot = oneof('?')  # Add as many characters as you want
 epsilon = lit('') # The pattern which matches empty string.
+
+def genseq(x, y, Ns, startx=0):
+    "Set of matches to xy whose total len in in Ns."
+    if not Ns:
+        return null
+    xmatches = x(set(range(startx, max(Ns)+1)))
+    Ns_x = set(len(m) for m in xmatches)
+    Ns_y = set(n-m for n in Ns for m in Ns_x if n-m >= 0)
+    ymatches = y(Ns_y)
+    return set(m1 + m2 
+               for m1 in xmatches for m2 in ymatches
+               if len(m1+m2) in Ns)
+
+def test_gen():
+    def N(hi): return set(range(hi+1))
+    a,b,c = map(lit, 'abc')
+    assert star(oneof('ab'))(N(2)) == set(['', 'a', 'aa', 'ab',
+                                           'ba', 'bb', 'b'])
+    return 'test_gen_passes'
+
