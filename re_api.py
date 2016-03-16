de15d96f8cd407f3bbe7e924d3fd00157812468a
saurabhs92 <saurabhs92@gmail.com>
2016-03-16 23:54:40 +0530

    Added call to update_wrapper to copy fn documentation to new fn.

--- re_api.py
+++ re_api.py
@@ -8,11 +8,14 @@ Regular Expressions APIs - Interpretter Implementation
 
 """
 
+from functools import update_wrapper
+
 def n_ary(f):
     """Given binary function f(x,y), return an n_ary function such that
     f(x,y,z) = f(x, f(y,z)), etc. Also allow f(x)=x"""
     def n_ary_f(x, *args):
         return x if not args else f(x, n_ary_f(*args))
+    update_wrapper(n_ary_f, f)
     return n_ary_f
 
 def lit(x):       return ('lit', x)
