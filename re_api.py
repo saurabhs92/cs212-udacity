f0847c0d0b3d4f37f66d49061f82f342973edd0a
saurabhs92 <saurabhs92@gmail.com>
2016-03-17 00:39:34 +0530

    Removed update_wrapper call from within n_ary fn. Added decorator fn.

--- re_api.py
+++ re_api.py
@@ -10,13 +10,20 @@ Regular Expressions APIs - Interpretter Implementation
 
 from functools import update_wrapper
 
+@decorator
 def n_ary(f):
     """Given binary function f(x,y), return an n_ary function such that
     f(x,y,z) = f(x, f(y,z)), etc. Also allow f(x)=x"""
     def n_ary_f(x, *args):
         return x if not args else f(x, n_ary_f(*args))
-    update_wrapper(n_ary_f, f)
     return n_ary_f
+
+def decorator(d):
+    """Make function d a decorator; d wraps a function fn.
+    Updates help of both the fn d and the fn that d is applied to."""
+    def _d(fn):
+        return update_wrapper(d(fn), d)
+    return update_wrapper(_d, d)
 
 def lit(x):       return ('lit', x)
 
